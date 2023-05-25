1d7d83971fd611d62477cc1ba51801838b80e938
MalinStal <malinstal.94@gmail.com>
2023-05-25 21:53:55 +0200

    added timestamps

--- todoapp.js
+++ todoapp.js
@@ -4,50 +4,68 @@ const description = document.getElementById("description");
 const ulTodo = document.getElementById("myUlTodo");
 const ulDone = document.getElementById("myUlDone");
 
+
 let todos = [];
+
+
 let todo = {
     addTask: toDoInput.value,
     description: description.value,
     bool: false,
+    time: setTime(),
 }
 todos.push(todo);
-console.log(todos)
+
+
 
 // skapa en ny li vid knapp tryckning
 addBtn.addEventListener("click", function() {
   todos.forEach(todo =>{
-    let li = document.createElement("li");
+    const li = document.createElement("li");
     ulTodo.append(li);
-    li.innerHTML= todo.addTask;
+    //li.innerHTML = todo.addTask;
+    
+    const title = document.createElement("h3");
+    li.appendChild(title);
+    title.innerHTML= todo.addTask;
 
-    let paragraf = document.createElement("p");
+    const paragraf = document.createElement("p");
     li.append(paragraf)
     paragraf.innerHTML = todo.description;
+
+    const timeStamp = document.createElement("span");
+    li.append(timeStamp);
+    timeStamp.innerHTML="added task" + todo.time;
     
-    let closeBtn = document.createElement("button");
+    const closeBtn = document.createElement("button");
     closeBtn.innerHTML = "X";
     li.append(closeBtn);
 
 
-    //toDoInput.value = "";
-   // description.value = "";
+   // toDoInput.value = "";
+    //description.value = "";
 
     //stryker över texten när en uppgift är klar och flytta till done list
     li.addEventListener("click" , e => {
-       // todo.done = false;
-      
-        li.style.textDecoration = "line-through";
+    
+        title.style.textDecoration = "line-through";
         ulDone.appendChild(li);
         todo.bool = true
+        
+        const timeStampDone = document.createElement("span");
+        li.append(timeStampDone) // skapar en tidsstämpel, behöver lösa så den försvinner om man vill ta tillbaka tasken till todolist
+        timeStampDone.innerHTML="done whit task" + todo.time;
         e.preventDefault()
       
         
-    });
+    }, {once : true}); //onse true betyder att detta event bara körs 1 gång sedan så går det inte att köra det igen. blir inte bra i detta läge 
+    
     //dubbelklicka för att ta bort sträcket om man behöver göra uppgiften igen och flytta tillbaka den till todo list
     li.addEventListener("dblclick", e => {
        
-            ulTodo.appendChild(li);
-        li.style.textDecoration = "none";
+        ulTodo.appendChild(li);
+        title.style.textDecoration = "none";
+       
         e.preventDefault()
         //todo.done = true;
         
@@ -67,8 +85,12 @@ addBtn.addEventListener("click", function() {
     
 
 }); 
-
-
+// funktion som sätter en tids stämpel
+function setTime() {
+    let currentTime = new Date().getTime();
+    let time = new Date(currentTime).toLocaleTimeString()
+        return time;
+    }
 
 
 // denna boolean ska på något sätt ha koll på om uppgiften är klar eller inte. 
