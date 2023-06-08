5f8a99c2d6e3c5bb63e52eb17a3b1cefc0c8ca58
MalinStal <malinstal.94@gmail.com>
2023-06-08 11:38:21 +0200

    get the boolean to work and added a errormessage

--- dummyJSTodo.js
+++ dummyJSTodo.js
@@ -10,12 +10,62 @@ fetch('https://dummyjson.com/todos')
 .then((value) => {
    
     for( let i =0; i < 5; i++) {
-       let li = document.createElement("li")
-       ulTodo.append(li)
-       li.innerHTML= value.todos.todo;
+       let todos =  value.todos
+       dummyJSTodo(todos)
     }
     console.log(value)
 });
+
+function dummyJSTodo(todo) {
+  const li = document.createElement("li");
+    ulTodo.append(li);
+    
+    const checkbox = document.createElement("input");
+    checkbox.type= "checkbox";
+    li.append(checkbox)
+    checkbox.classList.add("checkbox")
+    checkbox.checked = todo.completed;
+    const title = document.createElement("h3");
+    li.appendChild(title);
+    title.innerHTML= todo.todo;
+
+    const paragraf = document.createElement("p");
+    li.append(paragraf)
+    paragraf.innerHTML = todo.description;
+
+   // const timeStamp = document.createElement("span");
+    //li.append(timeStamp);
+    //timeStamp.classList.add("timestamps")
+    //timeStamp.innerHTML="added task  " + setTime();
+
+    //const timeStampDone = document.createElement("span");
+       // li.append(timeStampDone) 
+       // timeStampDone.classList.add("timestamps")
+    
+    const closeBtn = document.createElement("button");
+    closeBtn.innerHTML = "Delete";
+    li.append(closeBtn);
+    closeBtn.classList.add("closeBtn");
+
+   
+
+   //toDoInput.value = "";
+   // description.value = "";
+
+    checkbox.addEventListener("change", e => {
+      
+      if(checkbox.checked){
+        title.style.textDecoration = "line-through";
+        ulDone.appendChild(li);
+        //timeStampDone.innerHTML="  done whit task " + setTime();
+        } else{
+             ulTodo.appendChild(li);
+        title.style.textDecoration = "none";
+        //timeStampDone.innerHTML=" ";
+       
+        }
+    })
+}
 
 //bara klistrat in fetch för att läga till en todo
 fetch('https://dummyjson.com/todos/add', {
--- todoapp.js
+++ todoapp.js
@@ -1,6 +1,6 @@
 const addBtn = document.getElementById("btn");
 const toDoInput = document.getElementById("toDoInput");
-const description = document.getElementById("description");
+const descriptionInput = document.getElementById("description");
 const ulTodo = document.getElementById("myUlTodo");
 const ulDone = document.getElementById("myUlDone");
 const form = document.getElementById("form");
@@ -11,26 +11,30 @@ let todos = [];
 
 
 
-
 // skapa en ny li vid knapp tryckning
-addBtn.addEventListener("click", function() {
-  let todo = {
-    addTask: toDoInput.value,
-    description: description.value,
-    bool: false,
-  }
-todos.push(todo)
-//todos.forEach(todo =>{
-    const li = document.createElement("li");
+addBtn.addEventListener("click", function () {
+    let todo = {
+        addTask: toDoInput.value,
+        description: descriptionInput.value,
+        bool: false,
+    }
+    todos.push(todo)
+   
+    if(toDoInput.value === "" || descriptionInput.value === "") {
+        document.querySelector(".errorMessage").style.display = "block";
+    } else {
+        document.querySelector(".errorMessage").style.display = "none";
+         const li = document.createElement("li");
     ulTodo.append(li);
-    
+
     const checkbox = document.createElement("input");
-    checkbox.type= "checkbox";
+    checkbox.type = "checkbox";
     li.append(checkbox)
     checkbox.classList.add("checkbox")
+
     const title = document.createElement("h3");
     li.appendChild(title);
-    title.innerHTML= todo.addTask;
+    title.innerHTML = todo.addTask;
 
     const paragraf = document.createElement("p");
     li.append(paragraf)
@@ -39,64 +43,76 @@ todos.push(todo)
     const timeStamp = document.createElement("span");
     li.append(timeStamp);
     timeStamp.classList.add("timestamps")
-    timeStamp.innerHTML="added task  " + setTime();
+    timeStamp.innerHTML = "<strong>Added task: </strong>" + setTime();
 
     const timeStampDone = document.createElement("span");
-        li.append(timeStampDone) 
-        timeStampDone.classList.add("timestamps")
-    
+    li.append(timeStampDone)
+    timeStampDone.classList.add("timestamps")
+
     const closeBtn = document.createElement("button");
     closeBtn.innerHTML = "Delete";
     li.append(closeBtn);
     closeBtn.classList.add("closeBtn");
 
-    console.log(todos)
-
-   toDoInput.value = "";
+    toDoInput.value = "";
     description.value = "";
+ checkbox.addEventListener("change", e => {
+        if (checkbox.checked) {
+            title.style.textDecoration = "line-through";
+            paragraf.style.textDecoration = "line-through";
+            todo.bool = true;
 
-    //stryker över texten när en uppgift är klar och flytta till done list
-   
-    checkbox.addEventListener("change" , e => {
-       if(checkbox.checked){
-        title.style.textDecoration = "line-through";
-        ulDone.appendChild(li);
-        timeStampDone.innerHTML="  done whit task " + setTime();
-        } else{
-             ulTodo.appendChild(li);
-        title.style.textDecoration = "none";
-        timeStampDone.innerHTML=" ";
-       
+            ulDone.appendChild(li);
+            timeStampDone.innerHTML = "<strong>Done whit task: </strong>"  + setTime();
+        } else {
+            ulTodo.appendChild(li);
+            title.style.textDecoration = "none";
+            paragraf.style.textDecoration = "none";
+            timeStampDone.innerHTML = " ";
+            todo.bool = false;
+
+
         }
 
         timeStampDone.style = ""
         e.preventDefault()
-        
+
     });
-    
+
     //tar bort uppgiften när du klickar på x knappen
     closeBtn.addEventListener("click", e => {
-        if(li.parentElement === ulTodo) {
+        if (li.parentElement === ulTodo) {
             ulTodo.removeChild(li)
-        } 
-        else if(li.parentElement === ulDone)  {
+        }
+        else if (li.parentElement === ulDone) {
             ulDone.removeChild(li)
         }
-            e.stopPropagation()
-       
-    }) 
-//})
+        e.stopPropagation()
+
+    })
+    //})
+
+    }
+   
+    //stryker över texten när en uppgift är klar och flytta till done list
+
    
 
-}); 
+});
 // funktion som sätter en tids stämpel
 function setTime() {
     const now = new Date()
-    const hoursAndMinutes = now.getHours() + ':' + now.getMinutes();
-
-        return hoursAndMinutes;
-    }
-
+    let day = now.getDate().toString().padStart(2, "0");
+    let month = now.getMonth() + 1;
+    let month2 = month.toString().padStart(2, "0");
+    let year = now.getFullYear();
+    let currentDate = `${day}-${month2}-${year}`;
     
+    const hoursAndMinutes = now.getHours() + ':' + now.getMinutes().toString().padStart(2, "0");
+
+    return "Date: " + currentDate + " Time: " + hoursAndMinutes;
+}
+
+
 
 
